1e8868973bcf72c4c313bf8268b017f2f8884633
Lucas Beltner <the.real.beltner@gmail.com>
2026-03-25 19:51:40 +0100

    V1

---
+++ src/src/LibraryRoyaltyCalculator.java
@@ -0,0 +1,40 @@
+public class LibraryRoyaltyCalculator {
+
+    //HER STARTER DU KODEN DETTE ER DIN MAIN (hovedeblok)
+        public static void main(String[] args) {
+
+            //Test 1 Olga Ravn
+            // Celestine som printet bog: 166 sider SKØN (1.7) 140 eksemplarer
+            // Forventet point 166 × 1.7 × 140 = 39508
+            // Celestine som lydbog: 192 min, SKØN (1.7), 140 eksemplarer
+            // Forventet point: (192 × 0.5) × 1.7 × 140 = 22848
+            // Samlet forventet: (39508 + 22848) × 0.067574 = 4213.64 kr
+
+            Author olgaRavn = new Author("Olga Ravn");
+            olgaRavn.addTitle(new PrintedBook("Celestine", "SKØN", 140, 166));
+            olgaRavn.addTitle(new AudioBook("Celestine (lydbog)", "SKØN", 140, 192));
+            System.out.println(olgaRavn.getName() + ": " + olgaRavn.calculateRoyalties() + "kr");
+
+            //Test 2 Forfatter med blandede litteraturtyper
+            // Tester at convertLiteratureType() håndterer BI, LYRIK og FAG korrekt
+
+            Author kariDietz = new Author("Kari Dietz");
+            // Billedbog: 48 sider, BI (3.0), 220 eksemplarer
+            kariDietz.addTitle(new PrintedBook("Lille Frø finder hjem", "BI", 220, 48));
+            // Lyriksamling: 96 sider, LYRIK (6.0), 85 eksemplarer
+            kariDietz.addTitle(new PrintedBook("Saltmarsken taler", "LYRIK", 85, 96));
+            // Fagbog som lydbog: 410 min, FAG (1.0), 310 eksemplarer
+            kariDietz.addTitle(new AudioBook("Havets dyreliv (lydbog)", "FAG", 310, 410));
+            System.out.println(kariDietz.getName() + ": " + kariDietz.calculateRoyalties() + "kr");
+
+            //Test 3 Forfatter med kun lydbøger
+            // Sikrer at AudioBook-beregningen virker isoleret
+            Author mikkelBang = new Author("Mikkel Bang");
+            // Krimi som lydbog: 560 min, SKØN (1.7), 430 eksemplarer
+            mikkelBang.addTitle(new AudioBook("Mørket ved kysten (lydbog)", "SKØN", 430, 560));
+            // Tegneserie som lydbog: 95 min, TE (3.0), 180 eksemplarer
+            mikkelBang.addTitle(new AudioBook("Agent X fortæller (lydbog)", "TE", 180, 95));
+            System.out.println(mikkelBang.getName() + ": " + mikkelBang.calculateRoyalties() + "kr");
+        }
+    }
+
---
+++ src/src/maintester.java
@@ -0,0 +1,29 @@
+public class maintester {
+
+    /*
+    Author test = new Author("test");
+    test.addTitle(new PrintedBook("bog1", "SKØN", 100, 200));
+    System.out.println(test.calculateRoyalties());   // 2717.48 - virker
+
+    // hvad sker der hvis copies er 0?
+    Author test2 = new Author("test2");
+    test2.addTitle(new PrintedBook("bog", "SKØN", 0, 200));
+    System.out.println(test2.calculateRoyalties());  // 0.0
+
+    // prøver med lydbog
+    Author test3 = new Author("test3");
+    test3.addTitle(new AudioBook("lyd", "FAG", 50, 120));
+    System.out.println(test3.calculateRoyalties());  // 202.72
+
+    // test af BI og LYRIK
+    // PrintedBook pb = new PrintedBook("x", "BI", 10, 10);
+    // System.out.println(pb.calculatePoints());  // 300.0
+    // PrintedBook pb2 = new PrintedBook("x", "LYRIK", 10, 10);
+    // System.out.println(pb2.calculatePoints());  // 600.0
+
+    // hvad med en ukendt litteraturtype?
+    // Author test4 = new Author("test4");
+    // test4.addTitle(new PrintedBook("bog", "XYZ", 10, 10));
+    // test4.calculateRoyalties();  // kaster IllegalArgumentException
+*/
+}
